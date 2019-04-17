a9aa5475eeff9256966f0994c2a24411a7cb30d6
Andre Suzanovich <supius@gmail.com>
2019-04-17 11:25:34 +0200

    move AppBar to component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,42 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import IconButton from '@material-ui/core/IconButton';
-import Typography from '@material-ui/core/Typography';
-import InputBase from '@material-ui/core/InputBase';
-import MenuIcon from '@material-ui/icons/Menu';
-import SearchIcon from '@material-ui/icons/Search';
-import Item from './components/item'
+import Item from './components/Item'
+import NavBar from './components/NavBar'
 
 export default class App extends Component {
   render() {
     return (
       <div className="App">
-        <AppBar position="static">
-          <Toolbar>
-            <IconButton className="menuButton" color="inherit" aria-label="Open drawer">
-              <MenuIcon />
-            </IconButton>
-            <Typography variant="h6" color="inherit" noWrap>
-              Items shop
-            </Typography>
-            <div className="grow" />
-            <div className="search">
-              <div className="search-icon">
-                <SearchIcon />
-              </div>
-              <InputBase
-                placeholder="Search…"
-                classes={{
-                  root: "input-root",
-                  input: "input-input",
-                }}
-              />
-            </div>
-          </Toolbar>
-        </AppBar>
+        <NavBar />
         <Item description="Item 1"/>
       </div>
     );
---
+++ client/src/components/NavBar.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+
+const NavBar = () => {
+    return(
+        <AppBar position="static">
+            <Toolbar>
+                <Typography variant="title" color="inherit">
+                Items shop
+                </Typography>
+            </Toolbar>
+        </AppBar>
+    )
+}
+export default NavBar;
