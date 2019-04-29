dfa9f798e63a9a4fa2b1f66ffb0aa30085bda05b
Andre Suzanovich <supius@gmail.com>
2019-04-29 12:58:20 +0200

    add server side

---
+++ server/dist/schema.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const apollo_server_1 = require("apollo-server");
+const typeDefs = apollo_server_1.gql ``;
+exports.default = typeDefs;
+//# sourceMappingURL=schema.js.map
